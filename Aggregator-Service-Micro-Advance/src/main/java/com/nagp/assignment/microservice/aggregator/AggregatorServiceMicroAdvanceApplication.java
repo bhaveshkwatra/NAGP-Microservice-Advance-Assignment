42da599e279c701d3de543d6620b015b008448e2
bhaveshkwatra <bhaveshkwatra@host.docker.internal>
2020-04-05 21:30:32 +0530

    intial commit

--- Aggregator-Service-Micro-Advance/src/main/java/com/nagp/assignment/microservice/aggregator/AggregatorServiceMicroAdvanceApplication.java
+++ Aggregator-Service-Micro-Advance/src/main/java/com/nagp/assignment/microservice/aggregator/AggregatorServiceMicroAdvanceApplication.java
@@ -9,5 +9,4 @@ public class AggregatorServiceMicroAdvanceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AggregatorServiceMicroAdvanceApplication.class, args);
 	}
-
 }
